21c868c1802257aa6a7c2e264b650420fe022beb
byteland-app <byteland-app@users.noreply.github.com>
2026-02-03 02:31:59 +0000

    deploy: 1c038be46708f58fa0c8dfb4d8fd5b83c25fc168

---
+++ _next/static/tOapCMotD9RyHJzviuaQ-/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fbenchmark","\u002F[locale]\u002Fdocs\u002F[[...slug]]","\u002F[locale]\u002Fplayground","\u002Fdocs\u002F[[...slug]]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
